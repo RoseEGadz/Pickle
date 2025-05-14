1da27172b7b0f8dffec3e49f0cd53e569361c9d6
Eliza <livetolaff123@gmail.com>
2025-05-14 11:40:26 -0700

    finished events page

--- frontend/src/pages/events.jsx
+++ frontend/src/pages/events.jsx
@@ -1,9 +1,26 @@
-import { useEffect } from 'react';
-import { useParams, Link } from 'react-router'
+import { useState, useEffect } from 'react';
+import { useParams, Link, UNSAFE_getTurboStreamSingleFetchDataStrategy } from 'react-router'
 
 export default function Events() {
     const [events, setEvents] = useState([])
     const { id } = useParams();
+    const [imgUrl, setImgUrl] = useState('')
+
+    function getUrl() {
+        let imgUrl = ''
+        if (id == 1) {
+            imgUrl = 'https://static.vecteezy.com/system/resources/thumbnails/002/004/798/small/a-man-swimming-butterfly-vector.jpg'
+        } else if (id == 2) {
+            imgUrl = 'https://st4.depositphotos.com/2774203/23863/v/450/depositphotos_238632968-stock-illustration-backstroke-swim-sport-pool-competition.jpg'
+        } else if (id == 3) {
+            imgUrl = 'https://images.vexels.com/media/users/3/309119/isolated/preview/2b744adc2b02c2339cb8c74f8173fcba-close-up-of-a-swimmer-in-breaststroke.png'
+        } else if (id == 4) {
+            imgUrl = 'https://thumbs.dreamstime.com/b/circle-isolated-icon-logo-sport-swimming-swimmer-wave-water-blue-vector-187193130.jpg'
+        } else {
+            imgUrl = 'https://cbx-prod.b-cdn.net/COLOURBOX19380109.jpg?width=800&height=800&quality=70'
+        }
+        return imgUrl
+    }
 
     useEffect(() => {
         async function getEvents() {
@@ -13,7 +30,11 @@ export default function Events() {
                     throw new Error(request.status)
                 }
                 const data = await request.json()
-                setEvents(data)
+                const evies = data.filter(eve => eve.category_id == id)
+                console.log(evies)
+                setEvents(evies)
+                const url = getUrl()
+                setImgUrl(url)
             } catch(error) {
                 console.error(error)
             }
@@ -24,6 +45,24 @@ export default function Events() {
     return (
     <>
     <h1>Events</h1>
+    <img src={imgUrl} />
+    <div className='events'>
+    {events.map(eve => {
+        let olympic = ''
+        if (eve.olympic == true) {
+            olympic = 'Meters:'
+        } else {
+            olympic = 'Yards:'
+        }
+        const linky = `/events/${eve.id}`
+
+        return (
+            <Link to={linky}>
+            <button className='eve' key={eve.id}>{eve.name} {olympic}</button>
+            </Link>
+        )
+    })}
+    </div>
     <Link to='/categories'>
     <button>Back</button>
     </Link>
