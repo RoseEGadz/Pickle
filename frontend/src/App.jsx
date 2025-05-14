dc39e34d05ee041714c227ffa7567591055e8dfc
Eliza <livetolaff123@gmail.com>
2025-05-13 19:12:51 -0700

    made the first page and route stubs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router';
+import Main from './home.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+      </Routes>
+      <Footer />
     </>
   )
+}
+
+function Header() {
+  return (
+    <header>
+      <h2>The Pickle</h2>
+      <img className="icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxI-Z3m9VB4BGkOHCGiEhMEC9ihIkNIhX1bw&s"/>
+    </header>
+  )
+}
+
+function Footer(){
+  return (<p className="foot">Eliza Rose TM 2025</p>)
 }
 
 export default App
---
+++ frontend/src/images/images.js
@@ -0,0 +1,7 @@
+const images = {
+    "pretty icon": 'https://media.istockphoto.com/id/540365756/vector/swimming-man-splash-paint.jpg?s=612x612&w=0&k=20&c=ohaS3NjFLm6zqk5jFYNSwdkMCdrPqzsysgfIXguOWY8=',
+    "cool butterfly": "https://static.vecteezy.com/system/resources/thumbnails/002/004/798/small/a-man-swimming-butterfly-vector.jpg",
+    "circular icon": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxI-Z3m9VB4BGkOHCGiEhMEC9ihIkNIhX1bw&s",
+    "breastroke": "https://images.vexels.com/media/users/3/309119/isolated/preview/2b744adc2b02c2339cb8c74f8173fcba-close-up-of-a-swimmer-in-breaststroke.png",
+    "backstroke": "https://st4.depositphotos.com/2774203/23863/v/450/depositphotos_238632968-stock-illustration-backstroke-swim-sport-pool-competition.jpg"
+}
